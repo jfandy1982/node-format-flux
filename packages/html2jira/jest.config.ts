c207acaff117615d5ebf40019a7b555e3cea627e
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-04-05 13:39:43 +0200

    chore: :construction: setup build targets for library

---
+++ packages/html2jira/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'html2jira',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/html2jira',
+};
