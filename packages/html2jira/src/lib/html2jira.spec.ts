1763287d3d8507af4c623c6b62e6a38955c728f6
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-05-18 12:14:11 +0200

    feat: :tada: add a simple conversion for a <p>-tag with text only

--- packages/html2jira/src/lib/html2jira.spec.ts
+++ packages/html2jira/src/lib/html2jira.spec.ts
@@ -1,7 +1,10 @@
 import { html2jira } from './html2jira.js';
 
 describe('html2jira', () => {
-  it('should work', () => {
-    expect(html2jira()).toEqual('html2jira');
+  it('should receive undefined for an empty HTML', () => {
+    expect(html2jira('')).toBeUndefined();
+  });
+  it('should parse HTML to receive `Hello World!`', () => {
+    expect(html2jira('<p>Hello World!</p>')).toEqual('Hello World!');
   });
 });
--- packages/html2jira/src/lib/html2jira.ts
+++ packages/html2jira/src/lib/html2jira.ts
@@ -1,3 +1,17 @@
-export function html2jira(): string {
-  return 'html2jira';
+import parse, { DOMNode } from 'html-dom-parser';
+
+export function html2jira(htmlTextToParse: string): string | undefined {
+  const parsedObject: DOMNode[] = parse(htmlTextToParse);
+
+  for (const node of parsedObject) {
+    if (node.type === 'tag') {
+      for (const child of node.children) {
+        if (child.type === 'text') {
+          return child.data;
+        }
+      }
+    }
+  }
+
+  return undefined;
 }
